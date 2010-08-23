671263aeb0eed9e290bb603606fd5693c97fe7f0
Ales Zoulek <ales.zoulek@gmail.com>
2010-08-23 13:21:01 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+import os
+from setuptools import setup, find_packages
+
+setup(
+    name='django-constance',
+    version=os.environ.get('VERSION', '0.0.1'),
+    description='Django live settings stored in redis',
+    author='Comoga Django Team',
+    author_email='comoga@bitbucket.org',
+    license='GPL',
+    keywords='django libraries settings redis'.split(),
+    platforms='any',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+)
+
