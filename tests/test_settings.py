16b649fc4c59856e5ac4961fcf90f0862b8f677e
Jiri Barton <jbar@hosting4u.cz>
2010-09-03 14:38:43 +0200

    added tests for other types

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+from decimal import Decimal
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -15,5 +18,7 @@ CONSTANCE_CONFIG = {
     'INT_VALUE': (1, 'some int'),
     'BOOL_VALUE': (True, 'true or false'),
     'STRING_VALUE': ('Hello world', 'greetings'),
+    'DECIMAL_VALUE': (Decimal('0.1'), 'the first release version'),
+    'DATETIME_VALUE': (datetime(2010, 8, 23, 11, 29, 24), 'time of the first commit'),
+    'FLOAT_VALUE': (3.1415926536, 'PI'),
 }
-
--- tests/tests.py
+++ tests/tests.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+from decimal import Decimal
+
 from django.test import TestCase
 from django.conf import settings
 
@@ -15,15 +18,25 @@ class TestStorage(TestCase):
         self.assertEquals(config.INT_VALUE, 1)
         self.assertEquals(config.BOOL_VALUE, True)
         self.assertEquals(config.STRING_VALUE, 'Hello world')
+        self.assertEquals(config.DECIMAL_VALUE, Decimal('0.1'))
+        self.assertEquals(config.DATETIME_VALUE, datetime(2010, 8, 23, 11, 29, 24))
+        self.assertEquals(config.FLOAT_VALUE, 3.1415926536)
 
         # set values
         config.INT_VALUE = 100
         config.BOOL_VALUE = False
+        config.STRING_VALUE = 'Beware the weeping angel'
+        config.DECIMAL_VALUE = Decimal('1.2')
+        config.DATETIME_VALUE = datetime(1977, 10, 2)
+        config.FLOAT_VALUE = 2.718281845905
 
         # read again
         self.assertEquals(config.INT_VALUE, 100)
         self.assertEquals(config.BOOL_VALUE, False)
-        self.assertEquals(config.STRING_VALUE, 'Hello world')
+        self.assertEquals(config.STRING_VALUE, 'Beware the weeping angel')
+        self.assertEquals(config.DECIMAL_VALUE, Decimal('1.2'))
+        self.assertEquals(config.DATETIME_VALUE, datetime(1977, 10, 2))
+        self.assertEquals(config.FLOAT_VALUE, 2.718281845905)
 
     def test_nonexistent(self):
         try:
