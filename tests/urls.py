520d915527d536675f44ec8e26958b7663dc8548
Jannis Leidel <jannis@leidel.info>
2013-04-12 18:22:28 +0200

    Fixed import error.

--- tests/urls.py
+++ tests/urls.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from django.conf.urls import patterns, include
+
+try:
+    from django.conf.urls import patterns, include
+except ImportError:
+    from django.conf.urls.defaults import patterns, include
+
 
 urlpatterns = patterns('',
     (r'^admin/', include(admin.site.urls)),
