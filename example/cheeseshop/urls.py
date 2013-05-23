cf8f4da93d502fb073c5db35723bdafeb3f1197a
Bouke Haarsma <bouke@webatoom.nl>
2013-05-23 10:34:19 +0200

    Fixed admin statics in example project

--- example/cheeseshop/urls.py
+++ example/cheeseshop/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls.defaults import *
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -8,10 +10,20 @@ urlpatterns = patterns('',
     # Example:
     # (r'^cheeseshop/', include('cheeseshop.foo.urls')),
 
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
+    # Uncomment the admin/doc line below and add 'django.contrib.admindocs'
     # to INSTALLED_APPS to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
     (r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += patterns(
+        '',
+        url(r'^media/(?P<path>.*)$',
+            'django.views.static.serve', {
+                'document_root': settings.MEDIA_ROOT,
+            }),
+    )
