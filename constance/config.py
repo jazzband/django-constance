7dca4c5c517254c82806f66da9f3e91af881eff8
Lorenzo Colombini <lorenzo.colombini@populis.com>
2013-10-07 14:18:42 +0200

    deply internal cache of keys dict in config.py in order to minimize the number of query to backend; add RETRIEVE_ALL_KEYS (default: false) to enable added features)

--- constance/config.py
+++ constance/config.py
@@ -8,13 +8,20 @@ class Config(object):
     def __init__(self):
         super(Config, self).__setattr__('_backend',
             utils.import_module_attr(settings.BACKEND)())
+        if settings.RETRIEVE_ALL_KEYS:
+            super(Config, self).__setattr__('_keys',{})
+            for key,value in self._backend.mget(settings.CONFIG.keys()):
+                self._keys[key] = value
 
     def __getattr__(self, key):
         try:
             default, help_text = settings.CONFIG[key]
         except KeyError:
             raise AttributeError(key)
-        result = self._backend.get(key)
+        if settings.RETRIEVE_ALL_KEYS:
+            result = self._keys.get(key)
+        else:
+            result = self._backend.get(key)
         if result is None:
             result = default
             setattr(self, key, default)
@@ -24,6 +31,8 @@ class Config(object):
     def __setattr__(self, key, value):
         if key not in settings.CONFIG:
             raise AttributeError(key)
+        if settings.RETRIEVE_ALL_KEYS:
+            self._keys[key] = value
         self._backend.set(key, value)
 
     def __dir__(self):
