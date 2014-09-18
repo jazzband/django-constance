bf447288899dca11920831f3696e3da3b67098e1
qdonnellan <quentin@burkesoftware.com>
2014-09-18 15:27:30 -0500

    attempting a more elegant solution to detecting django version

--- constance/__init__.py
+++ constance/__init__.py
@@ -1,8 +1,11 @@
-from constance.config import Config
+import django
 
-try:
+major_version = django.VERSION[0]
+minor_version = django.VERSION[1]
+
+if major_version >= 1 and minor_version >= 7:
     from django.apps import AppConfig
-except ImportError:
-    config = Config()
-else:
     default_app_config = 'constance.apps.ConstanceConfig'
+else:
+    from constance.config import Config
+    config = Config()
