1f958dc4439fbe435b1d0381d15860708f1f9745
Jannis Leidel <jannis@leidel.info>
2014-11-25 22:46:58 +0100

    Make the config object lazy for old Djangos.
    This should prevent import time side effects from instantiating the config object directly there.

--- constance/__init__.py
+++ constance/__init__.py
@@ -1,10 +1,12 @@
 from .base import Config
+from django.utils.functional import SimpleLazyObject
 
 __version__ = '1.0a1'
+
 
 try:
     from django.apps import AppConfig  # noqa
 except ImportError:
-    config = Config()
+    config = SimpleLazyObject(Config)
 else:
     default_app_config = 'constance.apps.ConstanceConfig'
