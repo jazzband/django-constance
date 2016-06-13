54b6d55fd57b0f3e3effa5f2793984cef8cf1ad2
prokaktus <proktusfahitasiv@gmail.com>
2016-06-13 04:00:17 +0300

    Fix backend initialization in tests

--- tests/test_redis.py
+++ tests/test_redis.py
@@ -8,9 +8,9 @@ from tests.storage import StorageTestsMixin
 class TestRedis(StorageTestsMixin, TestCase):
 
     def setUp(self):
-        super(TestRedis, self).setUp()
         self.old_backend = settings.BACKEND
         settings.BACKEND = 'constance.backends.redisd.RedisBackend'
+        super(TestRedis, self).setUp()
         self.config._backend._rd.clear()
 
     def tearDown(self):
