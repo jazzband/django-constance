90bf97c0a938b922a147e0d187caa8ce7dd5a5f2
Camilo Nova <camilo.nova@axiacore.com>
2016-09-14 12:21:43 -0500

    Improved coding styles

--- constance/settings.py
+++ constance/settings.py
@@ -1,7 +1,10 @@
 from django.conf import settings
 
-BACKEND = getattr(settings, 'CONSTANCE_BACKEND',
-                  'constance.backends.redisd.RedisBackend')
+BACKEND = getattr(
+    settings,
+    'CONSTANCE_BACKEND',
+    'constance.backends.redisd.RedisBackend'
+)
 
 CONFIG = getattr(settings, 'CONSTANCE_CONFIG', {})
 
@@ -9,24 +12,34 @@ CONFIG_FIELDSETS = getattr(settings, 'CONSTANCE_CONFIG_FIELDSETS', {})
 
 ADDITIONAL_FIELDS = getattr(settings, 'CONSTANCE_ADDITIONAL_FIELDS', {})
 
-DATABASE_CACHE_BACKEND = getattr(settings, 'CONSTANCE_DATABASE_CACHE_BACKEND',
-                                 None)
+DATABASE_CACHE_BACKEND = getattr(
+    settings,
+    'CONSTANCE_DATABASE_CACHE_BACKEND',
+    None
+)
 
-DATABASE_CACHE_AUTOFILL_TIMEOUT = getattr(settings,
-                                          'CONSTANCE_DATABASE_CACHE_AUTOFILL_TIMEOUT',
-                                          60 * 60 * 24)
+DATABASE_CACHE_AUTOFILL_TIMEOUT = getattr(
+    settings,
+    'CONSTANCE_DATABASE_CACHE_AUTOFILL_TIMEOUT',
+    60 * 60 * 24
+)
 
 DATABASE_PREFIX = getattr(settings, 'CONSTANCE_DATABASE_PREFIX', '')
 
 REDIS_PREFIX = getattr(settings, 'CONSTANCE_REDIS_PREFIX', 'constance:')
 
-REDIS_CONNECTION_CLASS = getattr(settings, 'CONSTANCE_REDIS_CONNECTION_CLASS',
-                                 None)
+REDIS_CONNECTION_CLASS = getattr(
+    settings,
+    'CONSTANCE_REDIS_CONNECTION_CLASS',
+    None
+)
 
 REDIS_CONNECTION = getattr(settings, 'CONSTANCE_REDIS_CONNECTION', {})
 
 SUPERUSER_ONLY = getattr(settings, 'CONSTANCE_SUPERUSER_ONLY', True)
 
-IGNORE_ADMIN_VERSION_CHECK = getattr(settings,
-                                     'CONSTANCE_IGNORE_ADMIN_VERSION_CHECK',
-                                     False)
+IGNORE_ADMIN_VERSION_CHECK = getattr(
+    settings,
+    'CONSTANCE_IGNORE_ADMIN_VERSION_CHECK',
+    False
+)
