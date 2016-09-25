84aa49c698fd0e3e2643631061eb6baf1eaad0dc
John Carter <john@therefromhere.org>
2016-09-25 22:05:44 +1300

    Updated example app to django 1.8, added migrations

---
+++ example/cheeseshop/apps/storage/migrations/0001_initial.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Shelf',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=75)),
+            ],
+            options={
+                'verbose_name_plural': 'shelves',
+            },
+        ),
+        migrations.CreateModel(
+            name='Supply',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=75)),
+            ],
+            options={
+                'verbose_name_plural': 'supplies',
+            },
+        ),
+    ]
--- example/cheeseshop/urls.py
+++ example/cheeseshop/urls.py
@@ -1,22 +1,12 @@
-from django.conf.urls import *
+from django.conf.urls import include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.conf import settings
-
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
+from django.conf import settings
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Example:
-    # (r'^cheeseshop/', include('cheeseshop.foo.urls')),
-
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs'
-    # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    (r'^admin/', include(admin.site.urls)),
-)
+urlpatterns = [
+    url(r'^admin/', include(admin.site.urls)),
+]
 
 if settings.DEBUG:
     urlpatterns += staticfiles_urlpatterns()
