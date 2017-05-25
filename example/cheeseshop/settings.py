3af72e167ba54fa47c2cf81e4763ba3ef15b3efa
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2017-05-25 14:02:52 +0200

    Removed unused variables and imports.

--- example/cheeseshop/settings.py
+++ example/cheeseshop/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 from datetime import date
-from decimal import Decimal
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
