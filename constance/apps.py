2d86960ab9e505caf009f5841de181b0cd125d19
Camilo Nova <camilo.nova@axiacore.com>
2017-11-06 11:43:08 -0500

    Remove translation from the app name. Fixes #204

--- constance/apps.py
+++ constance/apps.py
@@ -1,11 +1,10 @@
 from django.db.models import signals
 from django.apps import AppConfig
-from django.utils.translation import ugettext_lazy as _
 
 
 class ConstanceConfig(AppConfig):
     name = 'constance'
-    verbose_name = _('Constance')
+    verbose_name = 'Constance'
 
     def ready(self):
         super(ConstanceConfig, self).ready()
