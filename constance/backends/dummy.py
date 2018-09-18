a785303b4d5e99a0b124c0704e6e82b38b651839
Viktor Danyliuk <v_danilyuk@wargaming.net>
2018-09-18 19:07:54 +0300

    Add DummyBackend.

---
+++ constance/backends/dummy.py
@@ -0,0 +1,17 @@
+from django.conf import settings
+
+from . import Backend
+from .. import settings as constance_settings
+
+
+class DummyBackend(Backend):
+
+    def mget(self, keys):
+        return {key: self.get(key) for key in keys}
+
+    def get(self, key):
+        return settings.CONSTANCE_CONFIG.get(key, (None,))[0]
+
+    def set(self, key, value):
+        """Don't do that!"""
+        raise NotImplementedError
