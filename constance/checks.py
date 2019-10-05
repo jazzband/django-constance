af9d8bbfc97ce7ffb73cb32d80fd1a6cfe2a01da
Dan Strohl <dstrohl@users.noreply.github.com>
2019-10-05 15:06:43 -0700

    Updated checks for additional detail and fixes
    added checks for no constance config, missing items, and dupe items.
    Also, the checks were not working for me, (not sure if that was because they were checking for "CONFIG" instead of "CONSTANCE_CONFIG", or if there was another reason).

--- constance/checks.py
+++ constance/checks.py
@@ -1,42 +1,77 @@
+from django.conf import settings
 from django.core import checks
 from django.utils.translation import ugettext_lazy as _
 
-from . import settings
+
+DUPE_ERROR = _('Duplicate Key found in Constance Config Fieldset: %r')
+DUPE_ERROR_HINT = _('Remove the duplicate key %r from CONFIG_FIELDSETS')
+DUPE_ERROR_ID = _('constance.E001')
+
+EXTRA_IN_FIELDSET = _('Extra Key found in Constance Config Fieldset not in CONFIG: %r')
+EXTRA_IN_FIELDSET_HINT = _('Remove the extra key %r from CONFIG_FIELDSETS')
+EXTRA_IN_FIELDSET_ID = _('constance.E003')
+
+MISSING_IN_FIELDSET = _('Key missing from Constance Config Fieldset: %r')
+MISSING_IN_FIELDSET_HINT = _('Add the missing key %r to CONFIG_FIELDSETS')
+MISSING_IN_FIELDSET_ID = _('constance.E003')
+
+NO_CONSTANCE_CONFIG = _('Constance is loaded, but CONSTANCE_CONFIG is setup in settings')
+NO_CONSTANCE_CONFIG_HINT = _('If you wish to use constance, create a CONSTANCE_CONFIG in settings, see https://django-constance.readthedocs.io/en/latest/')
+NO_CONSTANCE_CONFIG_ID = _('constance.E004')
 
 
-@checks.register("constance")
-def check_fieldsets(*args, **kwargs):
-    """
-    A Django system check to make sure that, if defined, CONFIG_FIELDSETS accounts for
-    every entry in settings.CONFIG.
-    """
-    if hasattr(settings, "CONFIG_FIELDSETS") and settings.CONFIG_FIELDSETS:
-        inconsistent_fieldnames = get_inconsistent_fieldnames()
-        if inconsistent_fieldnames:
-            return [
+@checks.register()
+def constance_config_check(app_configs, **kwargs):
+    errors = []
+    if 'CONSTANCE_CONFIG' not in settings:
+        errors.append(
+            checks.Warning(
+                NO_CONSTANCE_CONFIG,
+                hint=NO_CONSTANCE_CONFIG_HINT,
+                obj='settings.CONSTANCE_CONFIG',
+                id=NO_CONSTANCE_CONFIG_ID,
+            )
+        )
+        return errors
+    if 'CONSTANCE_CONFIG_FIELDSETS' not in settings:
+        return errors
+
+    existing_keys = set(settings.CONSTANCE_CONFIG.keys())
+    field_set_keys = set()
+    for fields_list in settings.CONSTANCE_CONFIG_FIELDSETS.values():
+        fields_list = set(fields_list)
+        overlap = field_set_keys.intersection(fields_list)
+        for field_name in overlap:
+            errors.append(
                 checks.Warning(
-                    _(
-                        "CONSTANCE_CONFIG_FIELDSETS is missing "
-                        "field(s) that exists in CONSTANCE_CONFIG."
-                    ),
-                    hint=", ".join(sorted(inconsistent_fieldnames)),
-                    obj="settings.CONSTANCE_CONFIG",
-                    id="constance.E001",
+                    DUPE_ERROR % field_name,
+                    hint=DUPE_ERROR_HINT % field_name,
+                    obj='Constance config (settings.CONFIG)',
+                    id=DUPE_ERROR_ID,
                 )
-            ]
-    return []
+            )
+        field_set_keys.update(set(fields_list))
+
+    for field_name in field_set_keys.difference(existing_keys):
+        errors.append(
+            checks.Error(
+                EXTRA_IN_FIELDSET % field_name,
+                hint=EXTRA_IN_FIELDSET_HINT % field_name,
+                obj='Constance config (settings.CONFIG)',
+                id=EXTRA_IN_FIELDSET_ID,
+            )
+        )
+
+    for field_name in existing_keys.difference(field_set_keys):
+        errors.append(
+            checks.Error(
+                MISSING_IN_FIELDSET % field_name,
+                hint=MISSING_IN_FIELDSET_HINT % field_name,
+                obj='Constance config (settings.CONFIG)',
+                id=MISSING_IN_FIELDSET_ID,
+            )
+        )
+
+    return errors
 
 
-def get_inconsistent_fieldnames():
-    """
-    Returns a set of keys from settings.CONFIG that are not accounted for in
-    settings.CONFIG_FIELDSETS.
-    If there are no fieldnames in settings.CONFIG_FIELDSETS, returns an empty set.
-    """
-    field_name_list = []
-    for fieldset_title, fields_list in settings.CONFIG_FIELDSETS.items():
-        for field_name in fields_list:
-            field_name_list.append(field_name)
-    if not field_name_list:
-        return {}
-    return set(set(settings.CONFIG.keys()) - set(field_name_list))
