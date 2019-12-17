6737469744c25386d504bd062e42bba654b43899
Bart Machielsen <bart@verticaldataanalytics.com>
2019-12-17 12:46:44 +0100

    get_item and set_item impl

--- constance/base.py
+++ constance/base.py
@@ -9,6 +9,12 @@ class Config(object):
         super(Config, self).__setattr__('_backend',
             utils.import_module_attr(settings.BACKEND)())
 
+    def __getitem__(self, key):
+        return self.__getattr__(key)
+    
+    def __setitem__(self, key, value):
+        return self.__setattr__(key, value)
+
     def __getattr__(self, key):
         try:
             if not len(settings.CONFIG[key]) in (2, 3):
