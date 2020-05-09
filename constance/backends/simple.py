45c9f9cad415e5be167f81766cfeac5a96741dbb
Misha Behersky <bmwant@gmail.com>
2020-05-09 11:19:20 +0300

    Add simple backend

---
+++ constance/backends/simple.py
@@ -0,0 +1,25 @@
+from . import Backend
+from .. import signals, config
+
+
+class SimpleBackend(Backend):
+
+    def __init__(self):
+        super().__init__()
+        self._storage = {}
+
+    def get(self, key):
+        return self._storage.get(key)
+
+    def mget(self, keys):
+        if not keys:
+            return
+        for key in keys:
+            yield key, self._storage.get(key)
+
+    def set(self, key, value):
+        old_value = self.get(key)
+        self._storage[key] = value
+        signals.config_updated.send(
+            sender=config, key=key, old_value=old_value, new_value=value
+        )
