a2592e010d9ccafb1f0bc20f6f4d9a2588387809
TamoshaytisV <amd.vladas@gmail.com>
2020-06-18 23:43:11 +0300

    do not configure pytest overrides if django is not configured

--- constance/test/pytest.py
+++ constance/test/pytest.py
@@ -6,6 +6,20 @@ Inspired by https://github.com/pytest-dev/pytest-django/.
 import pytest
 from contextlib import ContextDecorator
 from constance import config as constance_config
+from django.conf import settings
+
+
+def skip_if_django_not_configured():  # pragma: no cover
+    """
+    Raises a skip exception if no Django settings are available.
+    """
+    django_is_configured = bool(os.environ.get("DJANGO_SETTINGS_MODULE"))
+
+    if not django_is_configured and "django.conf" in sys.modules:
+        django_is_configured = sys.modules["django.conf"].settings.configured
+
+    if not django_is_configured():
+        pytest.skip("no Django settings")
 
 
 @pytest.hookimpl(trylast=True)
@@ -13,6 +27,8 @@ def pytest_configure(config):  # pragma: no cover
     """
     Register override_config marker.
     """
+    skip_if_django_not_configured()
+
     config.addinivalue_line(
         "markers",
         (
@@ -27,6 +43,8 @@ def pytest_runtest_call(item):  # pragma: no cover
     """
     Validate constance override marker params. Run test with overrided config.
     """
+    skip_if_django_not_configured()
+
     marker = item.get_closest_marker("override_config")
     if marker is not None:
         if marker.args:
@@ -65,6 +83,8 @@ class override_config(ContextDecorator):
         self._original_values = {}
 
     def __enter__(self):
+        skip_if_django_not_configured()
+
         self.enable()
 
     def __exit__(self, exc_type, exc_val, exc_tb):
@@ -76,4 +96,6 @@ def _override_config():
     """
     Make override_config available as a function fixture.
     """
+    skip_if_django_not_configured()
+
     return override_config
