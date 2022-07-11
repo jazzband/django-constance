f6ed61460ee73a0fa54494013a4c0103622dbb76
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2022-07-11 10:24:02 +0200

    Added Django 4.0 classifier.

--- setup.py
+++ setup.py
@@ -38,6 +38,7 @@ setup(
         'Framework :: Django :: 3.0',
         'Framework :: Django :: 3.1',
         'Framework :: Django :: 3.2',
+        'Framework :: Django :: 4.0',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
