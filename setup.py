7b59c6463584584c68c208096a400b0a1816478b
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2022-07-13 17:04:47 +0200

    Added support for Django 4.1.  (#487)
    * Added Django 4.0 classifier.
    
    * Added support for Django 4.1.

--- setup.py
+++ setup.py
@@ -38,6 +38,8 @@ setup(
         'Framework :: Django :: 3.0',
         'Framework :: Django :: 3.1',
         'Framework :: Django :: 3.2',
+        'Framework :: Django :: 4.0',
+        'Framework :: Django :: 4.1',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
