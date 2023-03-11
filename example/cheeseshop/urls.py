b4860568022edf8016985cd8346e85f5b6f46edb
Sergei Iurchenko <urchenko88@gmail.com>
2023-03-11 17:58:13 +0300

    fix_ci (#512)
    * fix_ci
    
    Co-authored-by: Iurchenko Sergei

--- example/cheeseshop/urls.py
+++ example/cheeseshop/urls.py
@@ -1,14 +1,13 @@
-from django.conf.urls import url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
 from django.conf import settings
-from django.urls import path
+from django.urls import re_path
 
 
 admin.autodiscover()
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    re_path('admin/', admin.site.urls),
 ]
 
 if settings.DEBUG:
