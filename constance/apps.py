d30759be5d94122054f95827e720d0654dcb3267
Alexandr Artemyev <mogost@gmail.com>
2024-07-04 14:15:41 +0500

    Fix

--- constance/apps.py
+++ constance/apps.py
@@ -6,6 +6,3 @@ class ConstanceConfig(AppConfig):
     name = 'constance'
     verbose_name = _('Constance')
     default_auto_field = 'django.db.models.AutoField'
-
-    def ready(self):
-        pass
