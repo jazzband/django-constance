2ae381e58ea6846c01663a38277d876a28486524
Pascal Fouque <pfouque@users.noreply.github.com>
2025-01-27 10:12:47 +0100

    Fix is_composite_pk on Django 5.2 (#608)

--- constance/admin.py
+++ constance/admin.py
@@ -169,6 +169,7 @@ class Config:
         verbose_name_plural = _('config')
         abstract = False
         swapped = False
+        is_composite_pk = False
 
         def get_ordered_objects(self):
             return False
