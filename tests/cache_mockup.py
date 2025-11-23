6a6a953682e0125f9293b0b63a513cf17eeac8da
Ivan <xmorozov@gmail.com>
2025-11-23 22:00:16 +0000

    tests

---
+++ tests/cache_mockup.py
@@ -0,0 +1,14 @@
+from typing import Any
+
+from django.core.cache.backends.base import BaseCache
+from django.core.cache.backends.locmem import LocMemCache
+
+
+class Cache(BaseCache):
+    def __init__(self, name, params: dict[str, Any]) -> None:
+        self._cache = LocMemCache(name, params)
+        self.add = self._cache.add
+        self.delete = self._cache.delete
+        self.set = self._cache.set
+        self.get = self._cache.get
+        self.clear = self._cache.clear
